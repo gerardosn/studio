c0ccf62b4fbb2cc9a8892a3f08ca605520ae7cbe
Armend Hollos <adondeestaelpiso@gmail.com>
2025-08-07 02:36:37 +0000

    header.tsx darle el ancho maximo de android large de figma

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ import {
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="container flex h-16 items-center" style={{ maxWidth: '412px' }}>
         <div className="flex-1 md:flex-grow-0">
           <Link href="/" className="flex items-center space-x-2">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M280-280q-33 0-56.5-23.5T200-360v-400q0-33 23.5-56.5T280-840h560q33 0 56.5 23.5T920-760v400q0 33-23.5 56.5T840-280H280Zm0-200v120h560v-120H698q-21 36-57 58t-81 22q-45 0-81-22t-57-58H280Zm280 0q33 0 56.5-23.5T640-560h200v-200H280v200h200q0 33 23.5 56.5T560-480Zm200 360H120q-33 0-56.5-23.5T40-200v-480h80v480h640v80ZM280-360h560-560Z"/></svg>
